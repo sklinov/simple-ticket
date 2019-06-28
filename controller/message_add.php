772bd3096eef94b8bc9cfda821040b44bdd617d9
sklinov <sergeyklinov@gmail.com>
2019-06-28 17:18:08 +0500

    Minor debug

--- controller/message_add.php
+++ controller/message_add.php
@@ -33,7 +33,7 @@
                 $uploadfile = $uploaddir.$ticket->user_id."_".time()."__".basename($_FILES['file']['name']);
                 var_dump($uploadfile);
                 if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
-                    $ticket->file_path = $uploadfile;
+                    $ticket->file_path = '/uploads/'.$ticket->user_id."_".time()."__".basename($_FILES['file']['name']);
                     $ticket->file_name = $_FILES['file']['name'];
                     echo "Файл ". $_FILES['file']['name'] ." был успешно загружен.\n";
                 } 
